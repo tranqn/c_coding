a36f099dca9e856a39e0344652327faa6f2aa15a
tran.qn <namquoctran24@gmail.com>
2024-06-18 19:32:44 +0200

    add project_2

---
+++ lvl_1/project_2/TQ.h
@@ -0,0 +1,18 @@
+#ifndef TQ_H
+#define TQ_H
+
+int getNumberFromUser();
+
+int minimum_c(int a, int b);
+
+int maximum_c(int a, int b);
+
+float mean_c(int a, int b);
+
+int minArray(int *a, int len);
+
+int maxArray(int *a, int len);
+
+float meanArray(int *a, int len);
+
+#endif
